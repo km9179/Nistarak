e3072f1ff7d41ab945304603e17f926bccfee1f0
forgotter <only2prakash@gmail.com>
2019-02-19 19:48:21 +0530

    Added Maps to git. Added basic layout of new disease report

---
+++ app/src/main/java/com/example/nistarak/ReportDiseaseCase.java
@@ -0,0 +1,13 @@
+package com.example.nistarak;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportDiseaseCase extends AppCompatActivity {
+    //TODO : Add maps fragment for location and implement the code
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_disease_case);
+    }
+}
