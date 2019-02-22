e04d8272b7b58ca1c796043d0f2c44b9bb252019
Khushbu <khushbu999@github.gov>
2019-02-22 11:09:23 +0530

    Khushbu: First commit.Heatmap added.

--- app/src/main/java/com/example/nistarak/ReportDiseaseCase.java
+++ app/src/main/java/com/example/nistarak/ReportDiseaseCase.java
@@ -3,6 +3,7 @@ package com.example.nistarak;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Address;
@@ -36,8 +37,16 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.maps.android.heatmaps.Gradient;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+import com.google.maps.android.heatmaps.WeightedLatLng;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 
@@ -59,6 +68,69 @@ public class ReportDiseaseCase extends AppCompatActivity implements OnMapReadyCa
     private LatLng patientLocation;
     private boolean firstTimeFlag = true;
 
+    //Places
+    private ArrayList<String> mPlacesArraylist = new ArrayList<>();
+    List<String> namesList = Arrays.asList( "Brisbane", "Sydney", "Melbourne", "Perth" ,"Darwin");
+
+    //Location
+    private static final LatLng BRISBANE = new LatLng(-27.47093, 153.0235);
+
+    private static final LatLng MELBOURNE = new LatLng(-37.81319, 144.96298);
+
+    private static final LatLng DARWIN = new LatLng(-12.4634, 130.8456);
+
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+    private static final LatLng ADELAIDE = new LatLng(-34.92873, 138.59995);
+
+    private static final LatLng PERTH = new LatLng(-31.952854, 115.857342);
+
+    private static final LatLng ALICE_SPRINGS = new LatLng(-24.6980, 133.8807);
+
+    private ArrayList<LatLng> mLatlngArraylist = new ArrayList<>();
+    List<LatLng> mLatlngList = Arrays.asList(BRISBANE,SYDNEY,MELBOURNE,PERTH,DARWIN);
+
+
+    private static final WeightedLatLng wBRISBANE = new WeightedLatLng(BRISBANE,50);
+
+    private static final WeightedLatLng wSYDENEY = new WeightedLatLng(SYDNEY,100);
+
+    private static final WeightedLatLng wMELBOURNE = new WeightedLatLng(MELBOURNE,150);
+
+    private static final WeightedLatLng wPERTH = new WeightedLatLng(PERTH,80);
+
+    private static final WeightedLatLng wDARWIN = new WeightedLatLng(DARWIN,200);
+
+    private ArrayList<WeightedLatLng> mWeightedLatlngArraylist = new ArrayList<>();
+
+    List<WeightedLatLng> mWeightedLatlngList = Arrays.asList(wBRISBANE,wSYDENEY,wMELBOURNE,wPERTH,wDARWIN);
+
+    //Diseases
+    ArrayList<String> mDiseaseArraylist = new ArrayList<>();
+    List<String> mDiseaseList = Arrays.asList("Malaria");
+
+    //Age
+    ArrayList<Integer> mAgeArraylist = new ArrayList<>();
+
+    //Marker
+    private Marker mPerth;
+
+    private Marker mSydney;
+
+    private Marker mBrisbane;
+
+    private Marker mDarwin;
+
+    private Marker mMelbourne;
+    ArrayList<Marker> mMarkerArrayList= new ArrayList<>();
+    List<Marker> mMarkerList = Arrays.asList(mBrisbane,mSydney,mMelbourne,mPerth,mDarwin);
+    HashMap<String,Marker> mMarkerMap = new HashMap<>();
+
+    //HeatMap
+    private HeatmapTileProvider mProvider;
+    private TileOverlay mOverlay;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,6 +140,13 @@ public class ReportDiseaseCase extends AppCompatActivity implements OnMapReadyCa
         etNameOfDisease = (EditText) findViewById(R.id.etNameOfDisease);
         etAdhaarOfPatient = (EditText) findViewById(R.id.etAdhaarOfPatient);
         etAgeOfPatient = (EditText) findViewById(R.id.etAgeOfPatient);
+
+        mLatlngArraylist.addAll(mLatlngList);
+
+        mWeightedLatlngArraylist.addAll(mWeightedLatlngList);
+
+        mMarkerArrayList.addAll(mMarkerList);
+        mPlacesArraylist.addAll(namesList);
 
         btnAddCase = (Button) findViewById(R.id.btnAddCase);
         btnAddCase.setOnClickListener(new View.OnClickListener() {
@@ -132,17 +211,56 @@ public class ReportDiseaseCase extends AppCompatActivity implements OnMapReadyCa
         }
     };
 
-    private final View.OnClickListener clickListener = view -> {
-        if (view.getId() == R.id.currentLocationImageButton && googleMap != null && currentLocation != null) {
-            animateCamera(currentLocation);
+    private final View.OnClickListener clickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.currentLocationImageButton && googleMap != null && currentLocation != null) {
+                ReportDiseaseCase.this.animateCamera(currentLocation);
+            }
         }
     };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
         this.googleMap = googleMap;
+        mAddMarkerToMap(mLatlngArraylist,mMarkerArrayList,mPlacesArraylist);
         googleMap.setOnMarkerDragListener(this);
+        addHeatMap();
     }
+
+
+    private void addHeatMap() {
+        Log.d("MapActivity: ","Heat Map.");
+        /*List<LatLng> list = null;
+
+        // Get the data: latitude/longitude positions of police stations.
+        try {
+            list = readItems(R.raw.police_stations);
+        } catch (JSONException e) {
+            Toast.makeText(this, "Problem reading list of locations.", Toast.LENGTH_LONG).show();
+        }*/
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+        int[] colors = {
+                Color.rgb(102, 225, 0), // green
+                Color.rgb(255, 0, 0)    // red
+        };
+
+        float[] startPoints = {
+                0.2f, 1f
+        };
+
+        Gradient gradient = new Gradient(colors, startPoints);
+
+        mProvider = new HeatmapTileProvider.Builder()
+                .weightedData(mWeightedLatlngArraylist)
+                .gradient(gradient)
+                .build();
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mProvider.setRadius(50);
+        mOverlay = googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    }
+
 
     private void startCurrentLocationUpdates() {
         LocationRequest locationRequest = LocationRequest.create();
@@ -190,6 +308,24 @@ public class ReportDiseaseCase extends AppCompatActivity implements OnMapReadyCa
     @NonNull
     private CameraPosition getCameraPositionWithBearing(LatLng latLng) {
         return new CameraPosition.Builder().target(latLng).zoom(16).build();
+    }
+
+    private void mAddMarkerToMap(ArrayList<LatLng> mArraylistLatlng,ArrayList<Marker> mMarkerArrayList,ArrayList<String> mPlacesArraylist)
+    {
+        Log.d("MapActivity","Markers Added");
+        for( int i=0;i<mArraylistLatlng.size();i++)
+        {
+            if(!mMarkerMap.containsKey(mPlacesArraylist.get(i))) {
+                mMarkerMap.put(mPlacesArraylist.get(i), googleMap.addMarker(new MarkerOptions().position(mArraylistLatlng.get(i))
+                        .title(mPlacesArraylist.get(i))
+                        .snippet("Population: 4,137,400")
+                        .draggable(true)));
+
+                Toast.makeText(this, "Size: " + mMarkerMap.size(), Toast.LENGTH_LONG).show();
+            }
+
+        }
+
     }
 
     private void showMarker(@NonNull Location currentLocation) {
